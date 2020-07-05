37736d1b9306c070783324b18f7541713b9b8264
thepuzzletechie <w3.author@gmail.com>
2020-07-05 23:20:10 +0530

    Updates

---
+++ precache-manifest.d57748dc292ebd55074896af3ff906eb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51932a52eaaa03ca18255fbdb88fb6b1",
+    "url": "/breaking/index.html"
+  },
+  {
+    "revision": "8c6301d5a21d676a628c",
+    "url": "/breaking/static/css/main.2bfeecf1.chunk.css"
+  },
+  {
+    "revision": "877d64ca0f081c57e3e3",
+    "url": "/breaking/static/js/2.98d40023.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking/static/js/2.98d40023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c6301d5a21d676a628c",
+    "url": "/breaking/static/js/main.fb383679.chunk.js"
+  },
+  {
+    "revision": "819f371cc033342fdc62",
+    "url": "/breaking/static/js/runtime-main.d277549a.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking/static/media/spinner.235b9b3e.gif"
+  }
+]);
